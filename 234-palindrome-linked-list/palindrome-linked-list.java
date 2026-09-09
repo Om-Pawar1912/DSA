81cfbbf97324949f3a830336de8c05acb1ac43f5
Om Pawar <ompawar9168@gmail.com>
2026-09-09 13:45:14 +0530

    Time: 7 ms (27.58%) | Memory: 99.4 MB (21.79%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,30 @@
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+       ListNode temp = head;
+
+       ArrayList<Integer> arr = new ArrayList<>();
+
+       for(temp = head; temp!=null; temp = temp.next){
+        arr.add(temp.val);
+       }
+
+       boolean ans = true;
+
+       int x = 0;
+       int y = arr.size()-1;
+       while(x<=y){
+        if(arr.get(x)!=arr.get(y))
+        {
+            ans = false;
+            return ans;
+        }
+        x++;
+        y--;
+       }
+
+
+        return ans;
+    }
+}
